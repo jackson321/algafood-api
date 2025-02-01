3ffa60234d3e5717cbc2a3d8f74aad5360d9e339
jackson321 <jacksondeandrada@hotmail.com>
2025-02-01 10:42:13 -0300

    5° commit - finalizado aula 3.04

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,21 +1,20 @@
 package com.algaworks.algafood.domain.model;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "tab_cozinhas")
-public class Cozinha {
+public class Restaurante {
 
 	@Id
 	private Long id;
 
-	@Column(name = "")
 	private String nome;
+
+	@Column(name = "taxa_frete")
+	private BigDecimal taxaFrete;
 
 	public Long getId() {
 		return id;
@@ -33,6 +32,14 @@ public class Cozinha {
 		this.nome = nome;
 	}
 
+	public BigDecimal getTaxaFrete() {
+		return taxaFrete;
+	}
+
+	public void setTaxaFrete(BigDecimal taxaFrete) {
+		this.taxaFrete = taxaFrete;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -46,7 +53,7 @@ public class Cozinha {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Cozinha other = (Cozinha) obj;
+		Restaurante other = (Restaurante) obj;
 		return Objects.equals(id, other.id);
 	}
 
